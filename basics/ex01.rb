cd35f4b017f9d8af5b0116b26d5b094d20c3a157
Bartosz Kolek <bartoszkolek@yahoo.com>
2022-09-24 12:37:45 +0000

    first commit

---
+++ basics/ex01.rb
@@ -0,0 +1,6 @@
+movies = {:Matrix => 1999, :Pulp_Fiction => 1993}
+puts movies[:Matrix]
+puts movies[:Pulp_Fiction]
+dates = [1999, 1993]
+puts dates[0]
+puts dates[1]
---
+++ basics/ex06.rb
@@ -0,0 +1,6 @@
+puts 1.2*1.2
+puts 100.25*100.25
+
+a = gets.chomp.to_i
+b = a * a
+puts b
